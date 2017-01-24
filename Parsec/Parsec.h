a4b31bd4e165de7a0698f723ebed408310013339
Kevin Lindkvist <kevin.lindkvist@gmail.com>
2017-01-23 23:33:49 -0800

    Initial Commit

---
+++ Parsec/Parsec.h
@@ -0,0 +1,19 @@
+//
+//  Parsec.h
+//  Parsec
+//
+//  Created by Kevin Lindkvist on 1/23/17.
+//  Copyright © 2017 lindkvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Parsec.
+FOUNDATION_EXPORT double ParsecVersionNumber;
+
+//! Project version string for Parsec.
+FOUNDATION_EXPORT const unsigned char ParsecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parsec/PublicHeader.h>
+
+
